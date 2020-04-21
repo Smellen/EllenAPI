c26c37cf4fd11e7cbed7c102341c181611a18c88
Smellen <eburke624@gmail.com>
2020-04-21 23:34:05 +0100

    Changing project format

--- SteamGameStatistics/SteamAPI/SteamAPIService.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SteamAPIService
-{
-    public class SteamAPIService
-    {
-    }
-}
--- SteamGameStatistics/SteamService/SteamService.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SteamService
-{
-    public class SteamService
-    {
-    }
-}
